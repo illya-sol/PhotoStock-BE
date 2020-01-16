f304ddafec1bb9441a08649370b0851ac3d68d97
xCrazyxx <xcrazycapx@gmail.com>
2020-01-16 23:46:03 +0200

    Added env to .gitgnore

---
+++ src/env.conf.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "port": 3001,
+  "jwt_secret": "secr33t",
+  "jwt_expires": 36000,
+  "uAccess": "36442df883ce3518400cd6363d24975e39b80cbb98ab1ce3366c677c827c5895",
+  "uSecret": "db468096b7a3ef6751aa23da227cc046d74a499220f600bc04101bdc4974dba4",
+  "secreat_air": "keyrt1x7OwrKVaxm0",
+  "db_base": "appsXqB5XaFWzOKnu"
+}
