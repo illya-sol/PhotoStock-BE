bc91e31573dfe1853baeb126b443c1564bf6e332
xCrazyxx <xcrazycapx@gmail.com>
2020-04-29 17:47:31 +0300

    cors

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use(morgan(format,{
 }))
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Expose-Headers', 'Content-Length');
