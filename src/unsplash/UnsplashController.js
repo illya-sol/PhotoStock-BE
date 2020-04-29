af539476479d60ea224e5cf708bff5b90edb90e5
xCrazyxx <xcrazycapx@gmail.com>
2020-04-29 22:27:07 +0300

    Fixed Promises

--- src/unsplash/UnsplashController.js
+++ src/unsplash/UnsplashController.js
@@ -1,5 +1,6 @@
 const Splash = require('unsplash-js').default;
 const toJson = require('unsplash-js').toJson;
+const util = require('util');
 
 global.fetch = require('node-fetch');
 
@@ -10,31 +11,38 @@ const unsplash = new Splash({
     secret: config.uSecret
 });
 
-exports.Random = async (req, res) =>{
-  await unsplash.photos.getRandomPhoto().then(toJson).then(json => {
-      res.status(201).send({photos: json});
+exports.Random = (req, res) =>{
+  new Promise((resolve, reject) => {
+    unsplash.photos.getRandomPhoto().then(toJson).then(json => {
+      return resolve(res.status(201).send({photos: json}));
     }).catch((err) => {
-      res.status(401).send({err});
+      return reject(res.status(401).send({err}));
     });
+  });
 }
 
-exports.listPhotos = async (req, res) =>{
-  await unsplash.photos.listPhotos(req.query.page, req.query.per_page, req.query.order).then(toJson).then(json => {
-      res.status(201).send({photos: json});
+exports.listPhotos = (req, res) =>{
+  new Promise((resolve, reject) => {
+    unsplash.photos.listPhotos(req.query.page, req.query.per_page, req.query.order).then(toJson).then(json => {
+      return resolve(res.status(201).send({photos: json}));
     }).catch((err) => {
-      res.status(401).send({err});
+      return reject(res.status(401).send({err}));
     });
+  });
 }
 
-exports.startPage = async (req, res) =>{
-  await unsplash.photos.listPhotos(1, 30).then(toJson).then(json => {
-      res.status(201).send({photos: json});
+exports.startPage = (req, res) =>{
+  new Promise((resolve, reject) => {
+    unsplash.photos.listPhotos(1, 30).then(toJson).then(json => {
+      return resolve(res.status(201).send({photos: json}));
     }).catch((err) => {
-      res.status(401).send({err});
+      return reject(res.status(401).send({err}));
     });
+  });
 }
 
 exports.SearchPhotos = async (req, res) =>{
+    console.log(req.query)
   await unsplash.search.photos(req.params.keyword, req.query.page, req.query.per_page).then(toJson).then(json => {
       res.status(201).send({photos: json})
     }).catch((err) => {
